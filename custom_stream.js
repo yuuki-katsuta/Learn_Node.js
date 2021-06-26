2d119ac3148d03f1dc400c5db641ea76dbb00ce7
yuuki-katsuta <reptpr0314@gmail.com>
2021-06-26 23:08:55 +0900

    カスタムストリーム作成(書き込み)

--- custom_stream.js
+++ custom_stream.js
@@ -2,9 +2,24 @@
 // 2. ランダム文字列を読み取る CustomReadableStream を作成
 
 const CustomReader = require('./customreader');
+//クラスなのでインスタンス化
 const reader = new CustomReader();
 
 reader.on('data', (chunk) => {
   console.log(chunk);
 });
+reader.resume();
+
+// 前提
+//   以下の2モジュールが準備されていること
+//   - randomstring.js
+//   - customreader.js
+
+// 課題
+//   1. 受け取ったストリームをコンソールに書き出す CustomWritableStream を作成
+
+const CustomWriter = require('./customwriter');
+const writer = new CustomWriter();
+
+reader.pipe(writer); //Pipeで読み取りと書き込みを結合 読み取ったデータ(chunk)をコンソールに出力
 reader.resume();
---
+++ customwriter.js
@@ -0,0 +1,29 @@
+const { Writable } = require('stream');
+
+var CustomWriter = class extends Writable {
+  //optionsがあるので親クラスに渡す
+  constructor(options) {
+    super(options || { decodeStrings: true });
+  }
+
+  _write(chunk, encoding, done) {
+    var text = '';
+
+    if (encoding === 'buffer') {
+      text = chunk.toString(this._writableState.defaultEncoding);
+    } else {
+      text = chunk;
+    }
+
+    try {
+      console.log(text);
+    } catch (error) {
+      done(error);
+      return;
+    }
+
+    done();
+  }
+};
+
+module.exports = CustomWriter;
